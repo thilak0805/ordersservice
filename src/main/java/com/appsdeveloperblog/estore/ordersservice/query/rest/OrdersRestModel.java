b286486ea66cb84d44af65c229d197636ab75ad2
thilak <daya.22.thilak@gmail.com>
2023-08-11 00:57:27 +0530

    orders service-vaidations handled

--- src/main/java/com/appsdeveloperblog/estore/ordersservice/query/rest/OrdersRestModel.java
+++ src/main/java/com/appsdeveloperblog/estore/ordersservice/query/rest/OrdersRestModel.java
@@ -2,10 +2,17 @@ package com.appsdeveloperblog.estore.ordersservice.query.rest;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class OrdersRestModel {
 
+    @NotBlank(message ="productid is required field")
     private String productId;
+    @Min(value=1, message = "quantity cannot be lower than 1")
+    @Max(value=5, message = "quanitity cannot be longer than 5")
     private int quantity;
     private String addressId;
 }
