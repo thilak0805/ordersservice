f86d78d0283961fe1a405c09e13655946856052f
thilak <daya.22.thilak@gmail.com>
2023-08-16 09:06:40 +0530

    Custom error message added

--- src/main/java/com/appsdeveloperblog/estore/ordersservice/core/errorhandling/OrderServiceErrorHandler.java
+++ src/main/java/com/appsdeveloperblog/estore/ordersservice/core/errorhandling/OrderServiceErrorHandler.java
@@ -9,17 +9,23 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.Date;
+
 @ControllerAdvice
 public class OrderServiceErrorHandler {
 
     @ExceptionHandler(value = IllegalStateException.class)
     public ResponseEntity<Object> handleIllegalStateException(IllegalStateException ex, WebRequest request){
-        return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+       // return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(value=Exception.class)
     public ResponseEntity<Object> handleOtherException(Exception ex, WebRequest request){
-        return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+        //  return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
